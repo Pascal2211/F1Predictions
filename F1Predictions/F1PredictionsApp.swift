acf86ed66b65a09daff2c73f15283b5e7d23e4ea
Pascal <psibondagara@outlook.com>
2024-08-14 11:06:39 +0200

    Initial Commit

---
+++ F1Predictions/F1PredictionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  F1PredictionsApp.swift
+//  F1Predictions
+//
+//  Created by Pascal Sibondagara on 02/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct F1PredictionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ F1Predictions/Views/HomeView.swift
@@ -0,0 +1,18 @@
+//
+//  HomeView.swift
+//  F1Predictions
+//
+//  Created by Pascal Sibondagara on 14/08/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
