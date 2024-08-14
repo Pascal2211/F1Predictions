9b16a4f520d8a8a98683a4538be348bcf6d4aadf
Pascal <psibondagara@outlook.com>
2024-08-14 11:30:37 +0200

    First commit always feels good

---
+++ F1Predictions/Views/MainView.swift
@@ -0,0 +1,18 @@
+//
+//  MainView.swift
+//  F1Predictions
+//
+//  Created by Pascal Sibondagara on 14/08/2024.
+//
+
+import SwiftUI
+
+struct MainView: View {
+    var body: some View {
+        Text("This is a formula one app")
+    }
+}
+
+#Preview {
+    MainView()
+}
